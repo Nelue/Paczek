10de64c21a83c861f329058cef50ae3130ac38ef
Nelue <michalpaczek95@gmail.com>
2018-07-03 19:01:45 +0200

    generic summary

---
+++ AcquireData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    public class AcquireSensorData
+    {
+        
+        public double ReadTemperatureData()
+        {
+            return 0;
+        }
+        public double ReadPreassureData()
+        {
+            return 0;
+        }
+        public double ReadWindSpeedData()
+        {
+            return 0;
+        }
+        public double ReadWindDirectionData()
+        {
+            return 0;
+        }
+        public double ReadRainLevelData()
+        {
+            return 0;
+        }
+    }
+}
---
+++ AnalyzeData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    class AnalyzeData
+    {
+        public double CalculateTemperature(double temperatureData)
+        {
+            return 0;
+        }
+
+        public double CalculatePreassure(double preassureData)
+        {
+            return 0;
+        }
+
+        public double CalculateWindSpeed(double windSpeedData)
+        {
+            return 0;
+        }
+
+        public double CalculateWindDirection(double windDirectionData)
+        {
+            return 0;
+        } 
+
+        public double CalculateRainLevel(double rainLevelData)
+        {
+            return 0;
+        }
+    }
+}
